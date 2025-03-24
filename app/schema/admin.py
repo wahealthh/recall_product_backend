20f336af206d181c21477c2ddba47202b298af15
0xnuru <0xnuru@gmail.com>
2025-03-24 13:58:41 +0100

    feat: add remove model validator for create admin

--- app/schema/admin.py
+++ app/schema/admin.py
@@ -13,28 +13,3 @@ class CreateAdmin(BaseModel):
 
     class Config:
         from_attributes = True
-
-    @model_validator(mode="after")
-    def verify_password_match(self):
-        password1 = self.password1
-        password2 = self.password2
-
-        if password1 is None or password2 is None:
-            raise ValueError("Both password fields must be provided.")
-
-        password = password1.get_secret_value()
-        confirm_password = password2.get_secret_value()
-
-        if password != confirm_password:
-            raise ValueError("The two passwords did not match.")
-
-        password_regex = (
-            """(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,20}"""
-        )
-
-        if not re.match(password_regex, confirm_password):
-            raise ValueError(
-                "Password must be at least 8 characters and include alphabets, numbers, and a special character."
-            )
-
-        return self
